8d66c32e852bd74a330b2e35e7475f3abf11f863
611432104-oss <611432104@gms.ndhu.edu.tw>
2026-06-30 18:28:29 +0800

    url.js

--- url.js
+++ url.js
@@ -2,4 +2,3 @@ const urls = [
   "https://docs.google.com/forms/d/e/1FAIpQLSfyRsU652bMt0_spE-XWenSqWTkWuN5kF2to3CSSsWJjAzryg/viewform?usp=header",
   "https://docs.google.com/forms/d/e/1FAIpQLSeIAE1Hk8qYT4XMP50SOIVve4DYTSEM_9vFpA-N0JLgYf-o8A/viewform?usp=header"
 ];
-
